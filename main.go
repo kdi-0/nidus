f46bc590a7c50ce2e70c274a4c234fb491902b33
kdi-0 <kerwin.dahomey@gmail.com>
2022-06-11 17:45:28 -0600

    finish and divide

--- main.go
+++ main.go
@@ -2,62 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 )
-
-func scan(folder string) {
-	/*
-		get a slice of strings from recursiveScanFolder
-		get path of the dotfile we're going to write to
-		we write the slice contents to the file
-	*/
-	fmt.Printf("Found folders:\n\n")
-	repositories := recursiveScanFolder(folder)
-	filePath := getDotFilePath()
-	addNewSliceElementsToFile(filePath, repositories)
-	fmt.Printf("\n\nSuccesfully added \n\n")
-}
-
-func scanGitFolders(folders []string, folder string) []string {
-	// trim the last '/'
-	folder = strings.TrimSuffix(folder, "/")
-
-	f, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-	files, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var path string
-
-	for _, file := range files {
-		if file.IsDir() {
-			path = folder + "/" + file.Name()
-			if file.Name() == "git" {
-				path = strings.TrimSuffix(path, "/.git")
-				fmt.Println(path)
-				folders = append(folders, path)
-				continue
-			}
-			if file.Name() == "vendor" || file.Name() == "node_modules" {
-				continue
-			}
-			folders = scanGitFolders(folders, path)
-		}
-	}
-	return folders
-}
-
-func stats(email string) {
-	print("stats")
-}
 
 func main() {
 	var folder string
